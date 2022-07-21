355054d286e0474cbdbe098a3bfc8dfcf9d37569
vicky <abhishekkumarthakur786@gmail.com>
2022-07-21 23:44:48 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PKDependencyInjector
+
+A description of this package.
---
+++ Tests/PKDependencyInjectorTests/PKDependencyInjectorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PKDependencyInjector
+
+final class PKDependencyInjectorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PKDependencyInjector().text, "Hello, World!")
+    }
+}
